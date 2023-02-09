bb0e7fa9b0b7cdfa2f4cb8b9be70135faea5a1e2
Maurice Rivers <mrivers5@ggc.edu>
2023-02-09 03:02:18 -0500

    commented line added

--- src/BMI.java
+++ src/BMI.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class BMI {
+/**
+ * @Class: BMI
+ * @Date: 07 February 2023
+ * @Version: 1.0
+ * @Author: Maurice Rivers
+ * @description: calculate BMI based on weight and height
+ */
+
+
+import java.util.Scanner;
+public class BMI {
+    public static void main(String[] args) {
+        Scanner Input = new Scanner(System.in);
+        System.out.println("How much do you weigh in LBS: ");
+        double WeightLBS = Input.nextDouble();
+        System.out.println("How tall are you in inches: ");
+        double HeightInches = Input.nextDouble();
+
+        double WeightKGS = WeightLBS * 0.4536;
+        double HeightMeters = HeightInches * 0.0254;
+
+        double BMI = (WeightKGS / HeightMeters) * (WeightKGS / HeightMeters);
+        //BMI = Weight(kilograms)/(height(meters))2
+
+        System.out.println("Your BMI is " + BMI);
+    }
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,28 +1,38 @@
 /**
- * @class: CompareTwoCities
- * @date: 07 February 2023
- * @
+ * @Class: Triangle
+ * @Date: 07 February 2023
+ * @Version: 1.0
+ * @Author: Maurice Rivers
+ * @description: Calculate the perimeter of a triangle,
+ * and test if the inputs are Valid or Invalid,
+ * if The input is valid if the sum of every pair of two edges is greater than the remaining edge.
  */
 
 
-
-
-
-
 import java.util.Scanner;
-public class AlphabeticalOrder {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the first city: ");
-        String city1 = input.nextLine();
-        System.out.println("Enter the second city: ");
-        String city2 = input.nextLine();
 
-        if(city1.compareToIgnoreCase(city2)< 0 ){
-            System.out.println("The cities in order are " + city1 + ", " + city2);
-        }
-        else{
-            System.out.println(city2 + " " + city1);
-        }
+public class Triangle {
+    public static void main(String[] args) {
+        java.util.Scanner Input = new java.util.Scanner(System.in);
+        System.out.println("Length of First edge of triangle: ");
+        double Edge1 = Input.nextDouble();
+        System.out.println("Length of Second edge of triangle: ");
+        double Edge2 = Input.nextDouble();
+        System.out.println("Length of Third edge of triangle: ");
+        double Edge3 = Input.nextDouble();
+
+
+        double perimeter = Edge1 + Edge2 + Edge3;
+        System.out.println("The perimeter of the triangle is: " + perimeter);
+
+        if (Edge1 + Edge2 > Edge3 && Edge1 + Edge3 > Edge2 && Edge2 + Edge3 > Edge1)
+            System.out.println("The input is Valid!");
+        //The input is valid if the sum of every pair of
+            // two edges is greater than the remaining edge.
+        else
+            System.out.println("The input is invalid!");
+
+
+
     }
 }
